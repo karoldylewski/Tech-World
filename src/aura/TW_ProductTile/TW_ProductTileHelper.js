b8003ce4ba5f97eef21c0030ddd115eacdf6a297
kdylewski <karol.dylewski@britenet.com.pl>
2019-07-10 08:04:03 +0200

    switch event sequence

--- src/aura/TW_ProductTile/TW_ProductTileHelper.js
+++ src/aura/TW_ProductTile/TW_ProductTileHelper.js
@@ -5,15 +5,16 @@
     displayDetails : function(component){
         let productId = component.get("v.item.product.Id");
         let productName = component.get("v.item.product.Nd");
-        let sendCarObjectEvent = $A.get("e.c:TW_ProductSelected");
-        sendCarObjectEvent.setParams({
-             "productObject" : component.get("v.item")
-        })
-        sendCarObjectEvent.fire();
         let urlEvent = $A.get("e.force:navigateToURL");
         urlEvent.setParams({
         	"url": "/product/"+productId+"/"+productName
         });
         urlEvent.fire();
+        let sendCarObjectEvent = $A.get("e.c:TW_ProductSelected");
+        sendCarObjectEvent.setParams({
+            "productObject" : component.get("v.item")
+        })
+        sendCarObjectEvent.fire();
+        console.log('sent all events');
     }
 })
