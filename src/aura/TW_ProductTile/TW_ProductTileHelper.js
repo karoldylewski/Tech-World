aad5c97241041a97540f8916bcdb34dd01d09215
kdylewski <karol.dylewski@britenet.com.pl>
2019-07-10 07:54:59 +0200

    app event with product object

--- src/aura/TW_ProductTile/TW_ProductTileHelper.js
+++ src/aura/TW_ProductTile/TW_ProductTileHelper.js
@@ -5,6 +5,11 @@
     displayDetails : function(component){
         let productId = component.get("v.item.product.Id");
         let productName = component.get("v.item.product.Nd");
+        let sendCarObjectEvent = $A.get("e.c:TW_ProductSelected");
+        sendCarObjectEvent.setParams({
+             "productObject" : component.get("v.item")
+        })
+        sendCarObjectEvent.fire();
         let urlEvent = $A.get("e.force:navigateToURL");
         urlEvent.setParams({
         	"url": "/product/"+productId+"/"+productName
