41b94e9366d67461f9a339a179997dec15b3e896
kdylewski <karol.dylewski@britenet.com.pl>
2019-07-10 07:47:14 +0200

    preparing car product cmp files

---
+++ src/aura/TW_ProductDetails/TW_ProductDetailsController.js
@@ -0,0 +1,8 @@
+/**
+ * Created by BRITENET on 10.07.2019.
+ */
+({
+    doInit: function(component, event, helper){
+            helper.getProductDetails(component);
+          }
+})
---
+++ src/aura/TW_ProductDetails/TW_ProductDetailsHelper.js
@@ -0,0 +1,8 @@
+/**
+ * Created by BRITENET on 10.07.2019.
+ */
+({
+    getProductDetails: function(component, event, helper){
+        console.log('get product details');
+    }
+})
