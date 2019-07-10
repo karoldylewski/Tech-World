2723b47dc009d9b420f661116db645aa2e493c7f
kdylewski <karol.dylewski@britenet.com.pl>
2019-07-10 08:50:55 +0200

    debugging disp details

--- src/aura/TW_ProductTile/TW_ProductTileHelper.js
+++ src/aura/TW_ProductTile/TW_ProductTileHelper.js
@@ -4,17 +4,24 @@
 ({
     displayDetails : function(component){
         let productId = component.get("v.item.product.Id");
-        let productName = component.get("v.item.product.Nd");
+        let productName = component.get("v.item.product.Name");
+        let productNameConvertedToUrl = productName.replace(/\s+/g, '-').toLowerCase();
         let productItemObject = component.get("v.item");
+        console.log('before url redirect '+productId+' , '+productNameConvertedToUrl);
         let urlEvent = $A.get("e.force:navigateToURL");
         urlEvent.setParams({
-        	"url": "/product/"+productId+"/"+productName
+        	"url": "/product/"+productId+"/"+productNameConvertedToUrl
+//        	url: "/product/01t2p000007p4KAAAY/samsung-galaxy-s8"
         });
         urlEvent.fire();
+        console.log('fired url redirect');
+
         let productSelectedEvent = $A.get("e.c:TW_ProductSelected");
         productSelectedEvent.setParams({
             productObject : productItemObject
         })
         productSelectedEvent.fire();
+
+        console.log('fired obj event');
     }
 })
